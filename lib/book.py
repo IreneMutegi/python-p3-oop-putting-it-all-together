4187381f7138f05d0258be00f3a63239ba6c5f57
Irene Mutegi <irene@Ms-Echo.>
2024-12-03 22:25:27 +0300

    initial commit

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self.title = title  # Title is always a string
+        self.page_count = page_count  # Calls the setter to validate page_count
+
+    # Page count getter
+    def get_page_count(self):
+        return self._page_count
+
+    # Page count setter
+    def set_page_count(self, value):
+        if isinstance(value, int):
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+
+    # Define property for page_count
+    page_count = property(get_page_count, set_page_count)
+
+    # Method to simulate turning the page
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand  # Brand is a simple attribute
+        self.size = size  # Calls the setter for validation
+
+    # Getter for size
+    def get_size(self):
+        return self._size
+
+    # Setter for size with validation
+    def set_size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+
+    # Define property for size
+    size = property(get_size, set_size)
+
+    # Method to cobble (repair) the shoe
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"  # Set condition to "New"
